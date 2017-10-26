e7bec0bd9a682586252161e1819c54625c98fcc9
13372038054 <550906320@qq.com>
2017-10-26 14:37:48 +0800

    fix copyrights controversy

--- app/src/main/java/com/thirtydegreesray/openhub/util/PrefHelper.java
+++ app/src/main/java/com/thirtydegreesray/openhub/util/PrefHelper.java
@@ -1,17 +1,7 @@
 /*
- *    Copyright 2017 ThirtyDegreesRay
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ *   Copyright (C) 2017 Kosh.
+ *   Licensed under the GPL-3.0 license.
+ *   (See the LICENSE(https://github.com/k0shk0sh/FastHub/blob/master/LICENSE) file for the whole license text.)
  */
 
 package com.thirtydegreesray.openhub.util;
@@ -27,10 +17,7 @@ import com.thirtydegreesray.openhub.AppApplication;
 
 /**
  * Created on 2017/8/3.
- *
- * @author ThirtyDegreesRay
  */
-
 public class PrefHelper {
 
     public final static int LIGHT = 0;
