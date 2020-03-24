10fbd54970e0c57ba906c4eb6abf6a5dfe2c5c10
smalldi <1753520469@qq.com>
2020-03-24 13:45:28 +0800

    迁移至AndroidX 语言设置添加Auto选项

--- app/src/androidTest/java/com/thirtydegreesray/openhub/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/thirtydegreesray/openhub/ExampleInstrumentedTest.java
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.thirtydegreesray.openhub", appContext.getPackageName());
     }
 }
